1e529236e7f14a5db41f1487a123a7b8a478abd2
Sahaj Bhatt <sahaj.bhatt@gatech.edu>
2016-03-11 14:32:30 -0500

    Initial Commit

---
+++ Article Summarizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Article Summarizer
+//
+//  Created by Sahaj Bhatt on 3/11/16.
+//  Copyright © 2016 Sahaj Bhatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Article SummarizerTests/Article_SummarizerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Article_SummarizerTests.swift
+//  Article SummarizerTests
+//
+//  Created by Sahaj Bhatt on 3/11/16.
+//  Copyright © 2016 Sahaj Bhatt. All rights reserved.
+//
+
+import XCTest
+@testable import Article_Summarizer
+
+class Article_SummarizerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
